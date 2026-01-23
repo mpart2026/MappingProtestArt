037b5ece3b7f3a9b9dc86b52e1e697ea6a163e2b
Lini Radhakrishnan <mpart2026@gmail.com>
2026-01-23 09:23:35 -0500

    updated basepath in observable config file

--- observablehq.config.js
+++ observablehq.config.js
@@ -3,6 +3,7 @@ export default {
   // The app's title; used in the sidebar and webpage titles.
   title: "Mapping Protest Art",
   
+  basePath: "/MappingProtestArt",
   // The pages and sections in the sidebar. If you don't specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
